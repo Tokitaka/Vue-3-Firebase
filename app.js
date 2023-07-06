2a872b81b23d73343c09319f945351dc1c421454
tokitaka <artigen3@gmail.com>
2023-07-06 23:02:16 +0900

    Methods & Click events & Conditional Rendering

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 // vue code 작성
 // create Vue App
 const app = Vue.createApp(
+    // component
     {
-        // data, functions
-        template: '<h2>I am the template</h2>'
+        data() {
+            return {
+                showBooks: true,
+                title: 'The final empire',
+                author: 'Brandon Sanderson',
+                age: 45,
+                x: 0,
+                y: 0
+            }
+        },
+        methods: {
+            changeTitle(title){
+                // this.title = 'Words of Romance'
+                this.title = title
+            },
+            toggleShowBooks(){
+                this.showBooks = !this.showBooks
+            },
+            handleEvent( e, data) {
+                console.log(e);
+                if (data) {
+                    console.log(data);
+                }
+            },
+            handleMousemove(e) {
+                this.x = e.offsetX
+                this.y = e.offsetY
+            }
+        }
     }
 )
 // mount Vue App to a certain DOM that have the id "APP"
 app.mount('#app')
-// 해당 태그 내부만 제어할 수 있음 
 
