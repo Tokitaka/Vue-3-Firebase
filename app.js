e28384305694d4ccec7cd0f65b72e78bb4247537
tokitaka <artigen3@gmail.com>
2023-07-06 23:37:36 +0900

    attribute binding

--- app.js
+++ app.js
@@ -3,13 +3,15 @@
 const app = Vue.createApp(
     // component
     {
+        // dynamic value 는 무조건 여기 data 에서 관리한다
         data() {
             return {
+                url: 'http://www.naver.com',
                 showBooks: true,
                 books: [
-                    {title: 'name of the wind', author: 'patrick rothfuss'},
-                    {title: 'the way of kings', author: 'brandon sanderson'},
-                    {title: 'the final empire', author: 'brandon sernderson'},
+                    {title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg'},
+                    {title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg'},
+                    {title: 'the final empire', author: 'brandon sernderson', img: 'assets/3.jpg'},
                 ]
             }
         },
